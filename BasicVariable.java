42abbc0cdd78d29827468df1b117f37b0e2f2bb7
user <yeethong02@yahoo.com>
2024-11-26 15:48:54 +0800

    first commit

---
+++ BasicVariable.java
@@ -0,0 +1,9 @@
+public class BasicVariable {
+    
+    public static void main(String[] args) {
+        String name = "Hello Java Class";
+        System.err.println("hello world");
+    }
+    
+}
+
